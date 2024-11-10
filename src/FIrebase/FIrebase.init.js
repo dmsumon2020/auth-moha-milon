d5715d252467cde45653b4f45ceefe6558dab568
dmsumon2020 <dmsumon2020@gmail.com>
2024-11-11 02:07:14 +0600

    added route

---
+++ src/FIrebase/FIrebase.init.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBl6ZW1XacuywZpvscnk3N10T8T4YZ2lhI",
+  authDomain: "auth-moha-milon-8db3c.firebaseapp.com",
+  projectId: "auth-moha-milon-8db3c",
+  storageBucket: "auth-moha-milon-8db3c.firebasestorage.app",
+  messagingSenderId: "147972285024",
+  appId: "1:147972285024:web:fc04cda3a7cca446ded905",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root from "./Components/Root/Root";
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
+import Register from "./Components/Register/Register";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
